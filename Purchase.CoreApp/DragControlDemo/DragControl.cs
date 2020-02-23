6c72b7952afb887cb64149088e1613db29ece082
a172586418 <392193761@qq.com>
2020-02-23 15:23:56 +0800

    C# UI Academy Drag control Gradient Control Round Button.

---
+++ Purchase.CoreApp/DragControlDemo/DragControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragControlDemo
+{
+    class DragControl : Component
+    {
+        private Control _HandleControl;
+
+        public Control SelectControl
+        {
+            get { return this._HandleControl; }
+            set
+            {
+                this._HandleControl = value; 
+                this._HandleControl.MouseDown += new MouseEventHandler(this.DragForm_MouseDown);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr handle, int msg, int wParams, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void DragForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            bool flag = e.Button == MouseButtons.Left;
+            if (flag)
+            {
+                DragControl.ReleaseCapture();
+                DragControl.SendMessage(this.SelectControl.FindForm().Handle, 161, 2, 0);
+            }
+        }
+    }
+}
---
+++ Purchase.CoreApp/RoundButtonDemo/RoundButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoundButtonDemo
+{
+    class RoundButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+
+            this.Region = new Region(gp);
+            base.OnPaint(pevent);
+        }
+    }
+}
