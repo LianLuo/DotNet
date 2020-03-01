d6f53da5f82feb497c06a74578341eb5f8e3695b
a172586418 <392193761@qq.com>
2020-03-01 22:46:05 +0800

    C# UI Add Demo of MCDonald's Add main view. Add menu add logo.

---
+++ Purchase.CoreApp/CircularFormDemo/MainView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircularFormDemo
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.MoveSidePanel(this.btnHome);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MoveSidePanel(Control btn)
+        {
+            this.panelHighLight.Top = btn.Top;
+            this.panelHighLight.Height = btn.Height;
+        }
+
+        private void btnEat_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnEat);
+        }
+
+        private void btnCollection_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnCollection);
+        }
+
+        private void btnDelivery_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnDelivery);
+        }
+
+        private void btnTakeAway_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnTakeAway);
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnPayment);
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnCustomer);
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            this.MoveSidePanel(this.btnHome);
+        }
+    }
+}
