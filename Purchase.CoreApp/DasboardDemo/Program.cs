30d20eeb19264482a25f3304b142ab600f408f23
a172586418 <392193761@qq.com>
2020-03-08 23:03:54 +0800

    C# UI Academy Avast View

--- Purchase.CoreApp/DasboardDemo/Program.cs
+++ Purchase.CoreApp/DasboardDemo/Program.cs
@@ -16,7 +16,7 @@ namespace DasboardDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DashbordFrm());
         }
     }
 }
