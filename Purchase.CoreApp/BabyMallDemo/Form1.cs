92bcbfa03da036a7e3dec87474a0847a44f54ebb
a172586418 <392193761@qq.com>
2020-03-11 22:45:06 +0800

    C# UI Academy add Baby App

---
+++ Purchase.CoreApp/BabyMallDemo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BabyMallDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer1.Start();
+        }
+
+        private int nextPicIndex=1;
+        private void NextPicture()
+        {
+            if(nextPicIndex == 10)
+            {
+                nextPicIndex = 1;
+            }
+            sidePic.ImageLocation = $"Images/{nextPicIndex}.jpg";
+            nextPicIndex++;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NextPicture();
+        }
+    }
+}
